11beae9a91324b57c504c745fbd0c0c5c6579731
Ravel Mesquita <ravels78@gmail.com>
2025-03-05 19:57:25 -0300

    Criando Model, Dtos

---
+++ UserAuth/Data/Dtos/CreateUsuarioDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAuth.Data.Dtos
+{
+    public class CreateUsuarioDto
+    {
+        [Required]
+        public string UserName { get; set; } // Nome do usuario
+        [Required]
+        public string RegistrationNumber { get; set; } // Matricula do usuario
+        [Required]
+        public DateTime DateOfBirth { get; set; } // Data de nascimento
+        [Required]
+        [DataType(DataType.Password)] // Tipo de dado "senha"
+        public string Password { get; set; } // Senha
+        [Required]
+        [Compare("Password")] // Comparação do Password
+        public string PasswordConfirmation { get; set; } // Confirmação da senha
+        [Required]
+        public DateTime RegistronDate { get; set; } = DateTime.Now; // Data de registro
+    }
+}
---
+++ UserAuth/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserAuth.Model
+{
+    public class User : IdentityUser
+    {
+        
+        [Required]
+        public string RegistrationNumber { get; set; } // Matricula do usuario
+        [Required]
+        public DateTime DateOfBirth { get; set; } // Data de nascimento
+        [Required]
+        public DateTime RegistronDate { get; set; } = DateTime.Now; // Data de registro
+
+        // Construtor da classe "User"
+        public User() : base() { }
+    }
+}
