85e672b37a477c3c4423f832744294801a92223d
Ravel Mesquita <ravels78@gmail.com>
2025-03-11 19:26:34 -0300

    Criando politica de acesso

---
+++ UserAuth/Controllers/AccessController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserAuth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccessController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            return Ok("Acesso Permitido");
+        }
+
+    }
+}
